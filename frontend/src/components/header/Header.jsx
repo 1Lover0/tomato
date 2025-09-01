290f02e47497788f8e82b5273f3e946c1a3a35e0
Deepu Singh <159556385+1Lover0@users.noreply.github.com>
2025-09-01 21:56:04 +0530

    new

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className='header'>
         <div className="header-contents">
             <h2>Order your favourite food here</h2>
-            <p>Choose from a diverse menu featuring a delectable a delectable array</p>
+            <p>Choose from a diverse menu featuring a delectable a delectable array of dishes crafted with the finest ingredients and culinary experts. Our mission is to satisfy your cravings and elevate your dining exoerience, one delicious meal at a time.</p>
             <button>View Menu</button>
         </div>
     </div>
